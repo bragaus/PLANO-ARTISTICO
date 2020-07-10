7aedd194837cfbe5715e0d87094a8244cc06b6f8
bragaus <mannoplay@gmail.com>
2020-07-10 08:39:28 -0300

    criei um estilo global

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Rotas from './rotas';
 import { Router } from 'react-router-dom';
 import history from './history';
 import { ProvedorDeAutorizacao } from './Contexto/ContextoDeAutorizacao';
+import EstiloGlobal from './styles/estiloGlobal';
 
 function App() {
 
-	return (
+	return (<>
 		<ProvedorDeAutorizacao>
 			<Router history={history}>
 				<Rotas />
 			</Router>
 		</ProvedorDeAutorizacao>
-    );	
+		<EstiloGlobal />
+    </>);	
 }
 
 export default App;
