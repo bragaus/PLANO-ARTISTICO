e39213cc5aadede09c3960670a4ccb57530f3796
bragaus <mannoplay@gmail.com>
2020-07-19 08:40:30 -0300

    Adicionei o componente de mira que segue o evento do mouse

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Rotas from './rotas';
 import { Router } from 'react-router-dom';
 import history from './history';
 import { ProvedorDeAutorizacao } from './Contexto/ContextoDeAutorizacao';
-import EstiloGlobal from './styles/estiloGlobal';
+import EstiloGlobal, { Mira } from './styles/estiloGlobal';
 
 function App() {
+
+	function quandoMover(e) {
+		const cursor = document.querySelector(".cursor")
+		cursor.style.left = `${e.pageX}px`
+		cursor.style.top = `${e.pageY}px`
+	}
+
+	window.addEventListener("mousemove", quandoMover)
 
 	return (<>
 		<ProvedorDeAutorizacao>
 			<Router history={history}>
+				<Mira className="cursor"/>	
 				<Rotas />
 			</Router>
 		</ProvedorDeAutorizacao>
