651bcf68c5c9826cfbeb169f84f3360ffac50a09
bragaus <mannoplay@gmail.com>
2020-07-27 08:57:53 -0300

    esqueci dessesssssssss pqp

---
+++ build/precache-manifest.c24c243977db66a05fc396c6ca2f3519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4e7ce28b547f060027e515dcab03dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a06c6a5116f27a387d6f",
+    "url": "/static/js/2.498f6dc5.chunk.js"
+  },
+  {
+    "revision": "d5de52e269d7d2856ad32d6249156346",
+    "url": "/static/js/2.498f6dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7a85e1c1ede70549dc",
+    "url": "/static/js/main.76592fdc.chunk.js"
+  },
+  {
+    "revision": "75926d34d4a366cc914e",
+    "url": "/static/js/runtime-main.7feaf9ab.js"
+  }
+]);
