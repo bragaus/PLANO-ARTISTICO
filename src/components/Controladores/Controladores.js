6b0e2fb2863cee0be139535eb9afc41a7ae38879
bragaus <mannoplay@gmail.com>
2020-08-24 13:57:01 -0300

    depois dessa consegui ver muita evolução desde que comecei a fazer o site BAGULHO DOIDO kkk terminando o almoço tenho q voltar pro trampss vlw flw de noite é nois

--- src/components/Controladores/Controladores.js
+++ src/components/Controladores/Controladores.js
@@ -36,158 +36,81 @@ export function zIndex(id_daArte, tipo, arte, setAuxiliar, auxiliar) {
     setAuxiliar(auxiliar + 1);        
 };
 
+
 const Controladores = ({ id_daArte, tipo, arte, setArte, auxiliar, setAuxiliar }) => {
 
-    // Funções para movimentação das artes
-    
-    function moverArteParaDireita(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {
+    function moverArteParaEsquerda(id_daArte) {
 
-            const mudarArte = arte.map(arte => {
-                if(arte.ID === id_daArte) {
-                    arte.esquerda = arte.esquerda + 5
-                }
-                return arte;
-            });
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.esquerda = arte.esquerda - 5
+            }
+            return arte;
+        });
 
-            console.log(mudarArte);
-            setArte(mudarArte);
+        setArte(moverArte);
+    };    
 
-            // arte.forEach(ilustracao => {
-            //     if (ilustracao.ID === id_daArte) {
-            //         ilustracao.esquerda = ilustracao.esquerda + 5
-            //     }
-            // });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.esquerda = arteDeCapa.esquerda + 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.esquerda = colagem.esquerda + 5
-                }
-            });            
-        }
-        // setAuxiliar(auxiliar + 1);       
+    function moverArteParaDireita(id_daArte) {
+
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.esquerda = arte.esquerda + 5
+            }
+            return arte;
+        });
+
+        setArte(moverArte);
     };
 
-    function moverArteParaEsquerda(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {        
-            arte.forEach(ilustracao => {
-                if (ilustracao.ID === id_daArte) {
-                    ilustracao.esquerda = ilustracao.esquerda - 5
-                }
-            });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.esquerda = arteDeCapa.esquerda - 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.esquerda = colagem.esquerda - 5
-                }
-            });            
-        }
-        setAuxiliar(auxiliar + 1);
+    function moverArteParaBaixo(id_daArte) {
+
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.baixo = arte.baixo + 5
+            }
+            return arte;
+        });
+
+        setArte(moverArte);
     };
     
-    function moverArteParaBaixo(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {        
-            arte.forEach(ilustracao => {
-                if (ilustracao.ID === id_daArte) {
-                    ilustracao.baixo = ilustracao.baixo + 5
-                }
-            });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.baixo = arteDeCapa.baixo + 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.baixo = colagem.baixo + 5
-                }
-            });            
-        }
-        setAuxiliar(auxiliar + 1);        
-    };
+    function moverArteParaCima(id_daArte) {
 
-    function moverArteParaCima(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {    
-            arte.forEach(ilustracao => {
-                if (ilustracao.ID === id_daArte) {
-                    ilustracao.baixo = ilustracao.baixo - 5
-                }
-            });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.baixo = arteDeCapa.baixo - 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.baixo = colagem.baixo - 5
-                }
-            });            
-        }
-        setAuxiliar(auxiliar + 1);        
-    };
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.baixo = arte.baixo - 5
+            }
+            return arte;
+        });
 
-    function aumentar(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {        
-            arte.forEach(ilustracao => {
-                if (ilustracao.ID === id_daArte) {
-                    ilustracao.largura = ilustracao.largura + 5
-                }
-            });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.largura = arteDeCapa.largura + 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.largura = colagem.largura + 5
-                }
-            });            
-        }
-        setAuxiliar(auxiliar + 1);        
+        setArte(moverArte);
     };
     
-    function diminuir(id_daArte, tipo) {
-        if (tipo === 'ILLUSTRATION') {        
-            arte.forEach(ilustracao => {
-                if (ilustracao.ID === id_daArte) {
-                    ilustracao.largura = ilustracao.largura - 5
-                }
-            });
-        } else if (tipo === 'ALBUM COVER') {
-            arte.forEach(arteDeCapa => {
-                if (arteDeCapa.ID === id_daArte) {
-                    arteDeCapa.largura = arteDeCapa.largura - 5
-                }
-            });            
-        } else if (tipo === 'COLLAGE') {
-            arte.forEach(colagem => {
-                if (colagem.ID === id_daArte) {
-                    colagem.largura = colagem.largura - 5
-                }
-            });            
-        }
-        setAuxiliar(auxiliar + 1);        
-    }; 
+    function aumentar(id_daArte) {
+
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.largura = arte.largura + 5
+            }
+            return arte;
+        });
+
+        setArte(moverArte);
+    };
+    
+    function diminuir(id_daArte) {
+
+        const moverArte = arte.map(arte => {
+            if(arte.ID === id_daArte) {
+                arte.largura = arte.largura - 5
+            }
+            return arte;
+        });
+
+        setArte(moverArte);    
+    };     
+
 
     const [ confirmacaoParaDeletar, setConfirmacaoParaDeletar ] = useState(false);
 
@@ -199,27 +122,27 @@ const Controladores = ({ id_daArte, tipo, arte, setArte, auxiliar, setAuxiliar }
                     <Lixeira />
                 </li>
 
-                <li onClick={() => moverArteParaEsquerda(id_daArte, tipo)}>
+                <li onClick={() => moverArteParaEsquerda(id_daArte)}>
                     <SetaEsquerda />
                 </li>                
 
-                <li onClick={() => moverArteParaDireita(id_daArte, tipo)}>
+                <li onClick={() => moverArteParaDireita(id_daArte)}>
                     <SetaDireita />
                 </li>
 
-                <li onClick={() => moverArteParaBaixo(id_daArte, tipo)}>
+                <li onClick={() => moverArteParaBaixo(id_daArte)}>
                     <SetaBaixo />
                 </li>
 
-                <li onClick={() => moverArteParaCima(id_daArte, tipo)}>
+                <li onClick={() => moverArteParaCima(id_daArte)}>
                     <SetaCima />
                 </li>
 
-                <li onClick={() => aumentar(id_daArte, tipo)}>
+                <li onClick={() => aumentar(id_daArte)}>
                     <LupaAumentar/>
                 </li>
 
-                <li onClick={() => diminuir(id_daArte, tipo)}>
+                <li onClick={() => diminuir(id_daArte)}>
                     <LupaDiminuir />
                 </li>
             </Lista>
@@ -239,3 +162,220 @@ const Controladores = ({ id_daArte, tipo, arte, setArte, auxiliar, setAuxiliar }
 }
 
 export default Controladores;
+
+
+// export function zIndex(id_daArte, tipo, arte, setAuxiliar, auxiliar) {
+//     if (tipo === 'ILLUSTRATION') {        
+//         arte.forEach(ilustracao => {
+//             if (ilustracao.ID === id_daArte) {
+//                 ilustracao.zIndex = ilustracao.zIndex + 1
+//             }
+//         });
+//     } else if (tipo === 'ALBUM COVER') {
+//         arte.forEach(arteDeCapa => {
+//             if (arteDeCapa.ID === id_daArte) {
+//                 arteDeCapa.zIndex = arteDeCapa.zIndex + 1
+//             }
+//         });   
+//     } else if (tipo === 'Colagem') {
+//         arte.forEach(colagem => {
+//             if (colagem.ID === id_daArte) {
+//                 colagem.zIndex = colagem.zIndex + 1
+//             }
+//         });      
+//     }
+//     setAuxiliar(auxiliar + 1);        
+// };
+
+// const Controladores = ({ id_daArte, tipo, arte, setArte, auxiliar, setAuxiliar }) => {
+
+//     // Funções para movimentação das artes
+    
+//     function moverArteParaDireita(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.esquerda = ilustracao.esquerda + 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.esquerda = arteDeCapa.esquerda + 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.esquerda = colagem.esquerda + 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);       
+//     };
+
+//     function moverArteParaEsquerda(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {        
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.esquerda = ilustracao.esquerda - 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.esquerda = arteDeCapa.esquerda - 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.esquerda = colagem.esquerda - 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);
+//     };
+    
+//     function moverArteParaBaixo(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {        
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.baixo = ilustracao.baixo + 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.baixo = arteDeCapa.baixo + 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.baixo = colagem.baixo + 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);        
+//     };
+
+//     function moverArteParaCima(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {    
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.baixo = ilustracao.baixo - 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.baixo = arteDeCapa.baixo - 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.baixo = colagem.baixo - 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);        
+//     };
+
+//     function aumentar(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {        
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.largura = ilustracao.largura + 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.largura = arteDeCapa.largura + 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.largura = colagem.largura + 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);        
+//     };
+    
+//     function diminuir(id_daArte, tipo) {
+//         if (tipo === 'ILLUSTRATION') {        
+//             arte.forEach(ilustracao => {
+//                 if (ilustracao.ID === id_daArte) {
+//                     ilustracao.largura = ilustracao.largura - 5
+//                 }
+//             });
+//         } else if (tipo === 'ALBUM COVER') {
+//             arte.forEach(arteDeCapa => {
+//                 if (arteDeCapa.ID === id_daArte) {
+//                     arteDeCapa.largura = arteDeCapa.largura - 5
+//                 }
+//             });            
+//         } else if (tipo === 'COLLAGE') {
+//             arte.forEach(colagem => {
+//                 if (colagem.ID === id_daArte) {
+//                     colagem.largura = colagem.largura - 5
+//                 }
+//             });            
+//         }
+//         setAuxiliar(auxiliar + 1);        
+//     }; 
+
+//     const [ confirmacaoParaDeletar, setConfirmacaoParaDeletar ] = useState(false);
+
+//     // Botões responsaveis por receber o id e o tipo e chamar as funções a cima
+//     return(<>
+//         <Section>
+//             <Lista>
+//                 <li onClick={() => setConfirmacaoParaDeletar(true)}>
+//                     <Lixeira />
+//                 </li>
+
+//                 <li onClick={() => moverArteParaEsquerda(id_daArte, tipo)}>
+//                     <SetaEsquerda />
+//                 </li>                
+
+//                 <li onClick={() => moverArteParaDireita(id_daArte, tipo)}>
+//                     <SetaDireita />
+//                 </li>
+
+//                 <li onClick={() => moverArteParaBaixo(id_daArte, tipo)}>
+//                     <SetaBaixo />
+//                 </li>
+
+//                 <li onClick={() => moverArteParaCima(id_daArte, tipo)}>
+//                     <SetaCima />
+//                 </li>
+
+//                 <li onClick={() => aumentar(id_daArte, tipo)}>
+//                     <LupaAumentar/>
+//                 </li>
+
+//                 <li onClick={() => diminuir(id_daArte, tipo)}>
+//                     <LupaDiminuir />
+//                 </li>
+//             </Lista>
+//         </Section>
+
+//         {confirmacaoParaDeletar && (
+//             <DeletarArte 
+//                 id={id_daArte} 
+//                 tipo={tipo}
+//                 setArte={setArte}
+//                 arte={arte}
+//                 setConfirmacaoParaDeletar={setConfirmacaoParaDeletar}
+//             />
+//         )} 
+
+//     </>)
+// }
+
+// export default Controladores;
